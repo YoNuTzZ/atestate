3a0f4d66fadd59b3cb63cf870f9f9a6401b15dda
YoNuTzZ <ionut.grt@gmail.com>
2014-03-23 13:12:51 +0200

    Final changes

--- bors/main.c
+++ bors/main.c
@@ -3,11 +3,11 @@
 #include <string.h>
 #include "libMatrix/matrici.h"
 
-void swap(double *matrixLine, size_t a, size_t b)
+void referenceSwap(double *a, double *b) 
 {
-	double tmp = matrixLine[a];
-	matrixLine[a] = matrixLine[b];
-	matrixLine[b] = tmp;
+	double tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 int comparaCrescator(double a, double b) 
@@ -31,13 +31,13 @@ void sortarePrinSelectie(double *matrixLine, size_t lineSize, int (*cmp)(double,
 	size_t minimum;
 
 	for(size_t j, i = 0; i < lineSize - 1; i++) {
-		minimum = matrixLine[i];
+		minimum = i;
 		for(j = i; j < lineSize; j++) {
 			if((*cmp)(matrixLine[j], matrixLine[minimum])) {
 				minimum = j;
 			}
 		}
-		swap(matrixLine, minimum, j);
+		referenceSwap(&matrixLine[minimum], &matrixLine[i]);
 	}
 }
 
@@ -45,6 +45,24 @@ void sorteazaMatriceaDupaLinii(struct Matrix *A, int (*cmp)(double, double))
 {
 	for(int i = 0; i < A->lines; i++) {
 		sortarePrinSelectie(A->value[i], A->lines, cmp);
+	}
+}
+
+void sorteazaMatriceaDupaColoane(struct Matrix *A, int(*cmp)(double, double)) 
+{
+	size_t minimum;
+
+	for(int i = 0; i < A->columns; i++) {
+		// selection sort 
+		for(int j = 0; j < A->lines; j++) {
+			minimum = j;
+			for(int k = j; k < A->lines; k++) {
+				if((*cmp)(A->value[minimum][i], A->value[k][i])) {
+					minimum = k;
+				}
+			}
+			referenceSwap(&A->value[minimum][i], &A->value[j][i]);
+		}
 	}
 }
 
@@ -77,52 +95,85 @@ int main()
 
 		A = create_matrix(lines, columns);
 		read_matrix(A);
+		printf("\n");
 
-		printf("ce operatie doriti s afaceti?\n");
-		printf("puteti sorta o matrice supa linii si colaone");
+		printf("ce operatie doriti sa faceti?\n");
+		printf("puteti sorta o matrice dupa linii si coloane, ");
 		printf("calcula inversa unei matrici si determinantul acesteia: ");
 
 		scanf("%s", comanda);
 
-		if(!strcmp(comanda, "sorteaza")) {
+		if(!strcmp(comanda, "sortare")) {
 			printf("cum doriti sa sortati matricea? Dupa linii sau dupa coloane?: ");
 			scanf("%s", comanda);
 			
 			if(!strcmp(comanda, "linii")) {
-				printf("cum doriti sa o sortati? Crescator sau descrescator: ");
+				printf("cum doriti sa o sortati? crescator sau descrescator: ");
 				scanf("%s", comanda);
 
 				if(!strcmp(comanda, "crescator")) {
-					//sorteazaMatriceaDupaLinii(A, comparaCrescator);
+					sorteazaMatriceaDupaLinii(A, comparaCrescator);
+					print_matrix(A);
 				}
 				else if(!strcmp(comanda, "descrescator")) {
 					sorteazaMatriceaDupaLinii(A, comparaDescrescator);
+					print_matrix(A);
 				}
 				else {
 					printf("nu v-am inteles comanda\n");
 				}
 			}
 			else if(!strcmp(comanda, "coloane")) {
-				// sorteaza dupa coloane test 
+				printf("cum doriti sa o sortati? crescator sau descrescator: ");
+				scanf("%s", comanda);
+
+				if(!strcmp(comanda, "crescator")) {
+					sorteazaMatriceaDupaColoane(A, comparaCrescator);
+					print_matrix(A);
+				}
+				else if(!strcmp(comanda, "descrescator")) {
+					sorteazaMatriceaDupaColoane(A, comparaDescrescator);
+					print_matrix(A);
+				}
+				else {
+					printf("nu v-am inteles comanda\n");
+				}
 			}
 			else {
 				printf("nu v-am inteles comanda");
 			}
 		}
-		else if (!strcmp(comanda, "determiant")) {
-			// determinant rules
+		else if (!strcmp(comanda, "determinant")) {
+			if(A->lines == A->columns) { 
+				A->determinant = get_determinant(A);
+				printf("determinantul este %lf\n", A->determinant);
+			}
+			else {
+				printf("doar matricile patratice au determiannt\n");
+			}
 		}
 		else if(!strcmp(comanda, "inversa")) {
-			// inverse rules
+			if(A->lines == A->columns) {
+
+				A->determinant = get_determinant(A);
+				compute_inverse(A);
+
+				printf("inversa matricii introduse de dumneavoastra este: \n");
+				print_matrix(A);
+			}
+			else {
+				printf("doar matricile patratice au inversa\n");
+			}
 		}
 		else {
 			printf("comanda dumneavoastra nu a fost inteleasa \n");
 		}
 
+		printf("doriti sa efectuati operatii si pe alta matrice?\n");
+		printf("scrieti da daca da sau nu daca vreti sa inchideti programul: ");
+		scanf("%s", stare);
 
-		print_matrix(A);
 		distroy_matrix(A);
-
 	}
 	while(!strcmp(stare, "da"));
 
